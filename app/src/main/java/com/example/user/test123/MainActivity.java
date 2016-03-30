df0d638884d834f947df9739a02154211b122b04
Yuan <kc3416696@yahoo.com.tw>
2016-03-30 13:58:05 +0800

    123

--- app/src/main/java/com/example/user/test123/MainActivity.java
+++ app/src/main/java/com/example/user/test123/MainActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MenuItem;ju
 
 public class MainActivity extends AppCompatActivity {
 123
